5e31cf21ec248ce988b3a8ec39b8ef1ec0d9653b
Vladimyr <vladimyrshcherbina@gmail.com>
2022-08-18 11:16:16 -0700

    about,home,footer,header

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+ 
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get gallary" do
+    get static_pages_gallary_url
+    assert_response :success
+  end
+
+  test "should get contacts" do
+    get static_pages_contacts_url
     assert_response :success
   end
 end
